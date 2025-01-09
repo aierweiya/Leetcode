26d579d5cb1ee243abed2ff6ba48559060a498aa
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-09 10:43:51 +0900

    Time: 2062 ms (15.88%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -4,7 +4,9 @@ SELECT
     ROUND(COUNT(r.user_id) / (SELECT COUNT(user_id) FROM Users) * 100, 2) AS percentage
 FROM
     Register AS r
-JOIN Users AS u ON r.user_id = u.user_id
-GROUP BY r.contest_id
-ORDER BY percentage DESC, r.contest_id ASC
-
+JOIN 
+    Users AS u ON r.user_id = u.user_id
+GROUP BY 
+    r.contest_id
+ORDER BY 
+    percentage DESC, r.contest_id ASC
