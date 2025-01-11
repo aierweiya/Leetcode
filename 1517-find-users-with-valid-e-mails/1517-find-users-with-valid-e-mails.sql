e95be5dc92e672107f2587762a5bdbbaffd2ec3b
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-11 09:39:48 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT
+    *
+FROM
+    Users
+WHERE
+    CASE 
+        WHEN LEFT(mail, 1) REGEXP '^[a-zA-Z]' 
+        AND mail REGEXP '^[a-zA-Z0-9./_-]+@leetcode\.com$'
+        THEN 1 ELSE 0 END
