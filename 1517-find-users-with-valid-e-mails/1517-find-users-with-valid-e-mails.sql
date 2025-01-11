950c9a5bce4494d8c03d100cd1b5ff9c237a81fd
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-11 09:46:47 +0900

    Time: 961 ms (48.88%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -5,6 +5,7 @@ FROM
     Users
 WHERE
     CASE 
-        WHEN LEFT(mail, 1) REGEXP '^[a-zA-Z]' 
+        WHEN RIGHT(mail, 13) = '@leetcode.com' 
+        AND LEFT(mail, 1) REGEXP '^[a-zA-Z]' 
         AND mail REGEXP '^[a-zA-Z0-9./_-]+@leetcode.com$'
         THEN 1 ELSE 0 END
