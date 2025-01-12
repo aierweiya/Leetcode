37ef89ffca737eed5c12d8570673d3f7f99cc79f
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-12 22:11:03 +0900

    Time: 1695 ms (34.36%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -6,6 +6,14 @@ SELECT
     price
 FROM
     Sales
-WHERE (product_id, year) IN (SELECT product_id, MIN(year) FROM Sales GROUP BY product_id)
+WHERE 
+    (product_id, year) IN (
+    SELECT 
+        product_id, MIN(year) 
+    FROM 
+        Sales 
+    GROUP BY 
+        product_id
+    )
 
 
