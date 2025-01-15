8bc2b79059ab742b7091eb0a54b0a1709fdc39b4
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-15 13:34:54 +0900

    Time: 970 ms (27.11%), Space: 0B (100%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,7 +1,20 @@
 # Write your MySQL query statement below
 SELECT
+    DISTINCT product_id,
+    10 AS price
+FROM
+    Products
+GROUP BY
+    product_id
+HAVING
+    MIN(change_date) > '2019-08-16' 
+
+#UNION 操作符用于将两个或多个 SELECT 语句的结果集合并成一个新的结果集。换句话说，它可以将多个查询的结果堆叠在一起。
+UNION
+
+SELECT
     product_id,
-    CASE WHEN change_date > '2019-08-16' THEN 10 ELSE new_price END AS price
+    new_price
 FROM
     Products
 WHERE (product_id, change_date) IN
@@ -15,9 +28,4 @@ WHERE (product_id, change_date) IN
             change_date <= '2019-08-16'
         GROUP BY 
             product_id
-    ) OR product_id IN (
-        SELECT product_id 
-        FROM Products 
-        WHERE change_date > '2019-08-16'
-        )
-GROUP BY product_id
+    )
