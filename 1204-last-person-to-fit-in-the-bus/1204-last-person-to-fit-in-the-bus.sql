beca9f17bd15c39fac38347354ec35df23caeba6
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-16 10:30:46 +0900

    Time: 2125 ms (21.2%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,14 +1,35 @@
 # Write your MySQL query statement below
+#Using OVER fuction
 SELECT
-    q1.person_name
+    person_name
 FROM 
-    QUEUE AS q1
-JOIN
+    #選取 person_name, turn 以及計算累積權重 cum。
+    (SELECT
+        person_name,
+        turn,
+        #根據 turn 欄位排序，並對 weight 欄位進行累積求和。
+        sum(weight) over (order by turn) AS cum 
+    FROM 
+        Queue
+    ) AS p1
+WHERE
+    cum <= 1000 
+ORDER BY
+    turn DESC 
+LIMIT 1;
+
+#------------------------------------------
+#Using JOIN fuction
+#SELECT
+#    q1.person_name
+#FROM 
+#    QUEUE AS q1
+#JOIN
     #這個聯結的目的是為了計算在 q1 的 turn 之前的所有人的 weight 總和。
-    QUEUE AS q2 ON q1.turn >= q2.turn
-GROUP BY 
-    q1.turn
-HAVING 
+#    QUEUE AS q2 ON q1.turn >= q2.turn
+#GROUP BY 
+#    q1.turn
+#HAVING 
     #計算每個分組中 q2.weight 的總和，並篩選出總和小於等於 1000 的分組。
-    SUM(q2.weight) <= 1000
-LIMIT 1
+#    SUM(q2.weight) <= 1000
+#LIMIT 1
