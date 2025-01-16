090f6bbc26bbcee8f3248b048dafb8b331550b91
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-16 10:09:05 +0900

    Time: 2697 ms (13.22%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
 SELECT
-    DISTINCT q1.person_name
+    q1.person_name
 FROM 
     QUEUE AS q1
-JOIN 
+JOIN
+    #這個聯結的目的是為了計算在 q1 的 turn 之前的所有人的 weight 總和。
     QUEUE AS q2 ON q1.turn >= q2.turn
 GROUP BY 
     q1.turn
 HAVING 
+    #計算每個分組中 q2.weight 的總和，並篩選出總和小於等於 1000 的分組。
     SUM(q2.weight) <= 1000
-ORDER BY 
-    SUM(q2.weight) DESC
 LIMIT 1
