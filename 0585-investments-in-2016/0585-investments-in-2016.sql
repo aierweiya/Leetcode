2b8684ab01ae0721f2b417573ecec1b0510b06d7
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-20 11:49:33 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,27 @@
+# Write your MySQL query statement below
+WITH same_2015 AS (SELECT
+    tiv_2015
+FROM
+    Insurance
+GROUP BY 
+    tiv_2015
+HAVING 
+    COUNT(tiv_2015)
+LIMIT 1),
+u_location AS (SELECT
+    CONCAT(lat, lon) AS location
+FROM
+    Insurance
+GROUP BY
+    location
+HAVING
+    COUNT(location) > 1
+)
+
+SELECT
+    SUM(tiv_2016) AS tiv_2016
+FROM
+    Insurance
+WHERE
+    tiv_2015 IN (SELECT tiv_2015 FROM same_2015)
+    AND CONCAT(lat, lon) NOT IN (SELECT location FROM u_location)
