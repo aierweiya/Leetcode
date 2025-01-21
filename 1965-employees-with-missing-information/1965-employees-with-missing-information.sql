1a72686715407cfaa306b92a64767c5a0203f339
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-21 17:13:53 +0900

    Time: 1002 ms (34.49%), Space: 0B (100%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,26 +1,17 @@
 # Write your MySQL query statement below
-WITH e AS (
-SELECT
-    e.employee_id
-FROM
-    Employees AS e
-JOIN
-    Salaries AS s ON e.employee_id = s.employee_id
-)
-
 SELECT
     employee_id
 FROM 
     Employees
 WHERE
-    employee_id NOT IN (SELECT * FROM e)
+    employee_id NOT IN (SELECT employee_id FROM Salaries)
 UNION
 SELECT
     employee_id
 FROM 
     Salaries
 WHERE
-    employee_id NOT IN (SELECT * FROM e)
+    employee_id NOT IN (SELECT employee_id FROM Employees)
 
 ORDER BY
     employee_id ASC
