0aef0a01bc7e255d834e1c181ccdf661670715db
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-24 08:55:52 +0900

    Time: 331 ms (92.39%), Space: 0B (100%) - LeetHub

--- 3421-find-students-who-improved/3421-find-students-who-improved.sql
+++ 3421-find-students-who-improved/3421-find-students-who-improved.sql
@@ -3,15 +3,16 @@ SELECT
     student_id,
     subject,
     first_score,
-    score AS latest_score
+    latest_score
 FROM
     (SELECT
         *,
-        FIRST_VALUE(score) OVER(PARTITION BY student_id, subject ORDER BY exam_date) AS first_score
+        FIRST_VALUE(score) OVER(PARTITION BY student_id, subject ORDER BY exam_date) AS first_score,
+        LAST_VALUE(score) OVER(PARTITION BY student_id, subject ORDER BY exam_date ASC ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS latest_score
     FROM
         Scores
     ) AS s
 WHERE
-    first_score < score
+    first_score < latest_score
 GROUP BY
-    student_id
+    student_id, subject
