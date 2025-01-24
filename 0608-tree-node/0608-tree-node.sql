1e8099a824df03961946a3847fc683383fa312ad
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-24 13:15:50 +0900

    Time: 1553 ms (5.06%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -2,7 +2,7 @@
 SELECT
     id,
     CASE 
-        WHEN p_id IS NULL OR p_id IS NULL AND id IN (SELECT p_id FROM Tree GROUP BY p_id) THEN 'Root'
+        WHEN p_id IS NULL THEN 'Root'
         WHEN id IN (SELECT p_id FROM Tree GROUP BY p_id) THEN 'Inner'
         ELSE 'Leaf' END AS type
 FROM
