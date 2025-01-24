917ee48ffac824389a8d1877ac921f79e013c377
aierweiya <58924467+aierweiya@users.noreply.github.com>
2025-01-24 22:42:54 +0900

    Time: 764 ms (63.49%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -6,6 +6,7 @@ FROM
     Trips
 WHERE
     client_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
+    AND driver_id NOT IN (SELECT users_id FROM Users WHERE banned = 'Yes')
     AND request_at BETWEEN "2013-10-01" AND "2013-10-03"
 GROUP BY
     request_at
